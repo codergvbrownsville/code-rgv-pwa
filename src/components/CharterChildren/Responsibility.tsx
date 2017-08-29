e9e0e242624bc131a598e4f0048528cf049fe062
piq9117 <piq9117@gmail.com>
2017-08-28 20:48:58 -0500

    changed to compounded component

--- src/components/CharterChildren/Responsibility.tsx
+++ src/components/CharterChildren/Responsibility.tsx
@@ -21,32 +21,24 @@ class Title extends React.PureComponent<Children<string>> {
   }
 }
 
-class StringItems extends React.PureComponent<Children<string[]>> {
-  public render() {
+class Responsibilities extends React.PureComponent<
+  Children<(string | JSX.Element)[]>
+> {
+  public return() {
     return (
-      <span>
-        {this.props.children.map(item =>
-          <li key={v4()} className="subtitle is-6">
-            {item}
-          </li>
-        )}
-      </span>
+      <ul>
+        {this.props.children}
+      </ul>
     );
   }
 }
 
-type ItemsProp = {
-  items: string[];
-};
-
-class ResponsibilityItems extends React.PureComponent<ItemsProp> {
+class Item extends React.PureComponent<Children<string>> {
   public render() {
     return (
-      <List>
-        <StringItems>
-          {this.props.items}
-        </StringItems>
-      </List>
+      <li className="subtitle is-6">
+        {this.props.children}
+      </li>
     );
   }
 }
@@ -67,7 +59,55 @@ export class Responsibility extends React.PureComponent {
       <MarginTop>
         <Content>
           <Title>Committee Responsibility</Title>
-          <ResponsibilityItems items={responsibilities} />
+          <Responsibilities>
+            <Item>
+              Develop and recommend to the Board of Directors policies and
+              procedures governing the programs and services to be offered by
+              Code RGV and the Program to subscribing members and the public.
+            </Item>
+            <Item>
+              Provide policy guidance and consultation for the Executive
+              Director in setting priorities for programs and services.
+            </Item>
+            <Item>
+              Develop and ensure a program evaluation process that measures the
+              extent to which the Program is achieving its mission.
+            </Item>
+            <Item>
+              Regularly uses the evaluation data to inform and continuously
+              improve NHCN programs.
+            </Item>
+            <Item>
+              Coordinate the assignment of activities to committee members and
+              volunteers.
+            </Item>
+            <Item>
+              Establish necessary timelines based on member input and follow up
+              on assigned responsibilities.
+            </Item>
+            <Item>
+              Schedule and develop an agenda for meetings based on member and
+              volunteer input.
+            </Item>
+            <Item>
+              Prepare an annual budget, and prepare presentation for the board.
+            </Item>
+            <Item>
+              Prepare a monthly report provided to the board of directors.
+            </Item>
+            <Item>
+              Ensure the meeting minutes are recorded, completed and distributed
+              electronically to the Board in a timely manner.
+            </Item>
+            <Item>
+              Distribute the agenda with minutes to committee members one week
+              prior to each scheduled meeting.
+            </Item>
+            <Item>
+              Work with the Development Committee to solicit funding and
+              resources necessary to implement the Program.
+            </Item>
+          </Responsibilities>
         </Content>
       </MarginTop>
     );

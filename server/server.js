cef5105a829278ae75d419b3a5bc6f24e1e2fc6a
Erik Lopez <erik@eriklopez.work>
2017-08-29 22:35:05 -0500

    Add Express server

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('dist'));
+
+app.listen(8080, function() {
+    console.log('App started on port 8080!');
+});
